b8a635a8771ed6331dc122daae3073dcbd9ca11c
lecanardjaune <ugiraud@edenschool.fr>
2022-02-02 11:48:20 +0100

    fonctionnement complé du bouton en js + style css amélioré

--- Quizz/js/app.js
+++ Quizz/js/app.js
@@ -1,20 +1,25 @@
 const goodRep = document.getElementsByClassName("goodRep");
 const btn = document.querySelector("#btn");
-// const div = document.getElementsByName("div")
-
-// div.style.backgroundColor = "#ffb8b8";
-
-function goodResponse(){
-
-}
+const q = document.getElementsByClassName("q");
+const result = document.getElementById("result");
 
 btn.addEventListener('click',function(){
+    let goodR = 0;
+    let badR = 0;
+    let allR = 0;
     for(let i = 0; i < goodRep.length; i++){
         if(goodRep[i].checked){
             console.log("test");
+            q[i].style.background = "#90ee8f";
+            goodR++;
         }
-        else {
-
+        else if(!goodRep[i].checked) {
+            q[i].style.background = "#ffb8b8";
+            badR++;
         }
     }
-} );
+    allR = goodR + badR;
+    result.textContent = "vous avez " + goodR + "/" + allR + " bonne réponse !"
+    
+});
+
